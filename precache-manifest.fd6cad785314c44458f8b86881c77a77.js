ca5159738478c6dfb859fd3270766e309f608e60
Michael <michaelwoo921@gmail.com>
2020-07-28 10:49:37 -0700

    Updates

---
+++ precache-manifest.fd6cad785314c44458f8b86881c77a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e762e9abbf5830c986040c518fa849",
+    "url": "/myapps/index.html"
+  },
+  {
+    "revision": "319e4b84f487ae5f024e",
+    "url": "/myapps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "731ed5c5bd60f27201d1",
+    "url": "/myapps/static/js/2.381c04a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapps/static/js/2.381c04a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319e4b84f487ae5f024e",
+    "url": "/myapps/static/js/main.35f5a59c.chunk.js"
+  },
+  {
+    "revision": "b041d193714966d8ad34",
+    "url": "/myapps/static/js/runtime-main.f0957297.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapps/static/media/logo.5d5d9eef.svg"
+  }
+]);
